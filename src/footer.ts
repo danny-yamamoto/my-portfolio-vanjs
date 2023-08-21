3a6d615ce8575b3c697b1e98935b69bc1415d3b0
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-08-21 10:21:31 +0000

    Format css.

--- src/footer.ts
+++ src/footer.ts
@@ -2,20 +2,6 @@ import van from "vanjs-core"
 
 const { p, div, a, i } = van.tags
 
-/**
-        <footer>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>
-        <p>&copy; 2023 {head.title}. All rights reserved.</p>
-        <div class={styles["social-links"]}>
-            <a href="https://github.com/danny-yamamoto" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://twitter.com/dai_s_a_n" target="_blank"><i class="fab fa-twitter"></i></a>
-        </div>
-        </footer>
-
-    let twitterProfile = 'https://twitter.com/dai_s_a_n';
-    let githubProfile = 'https://github.com/danny-yamamoto';
-*/
-
 export const copyrights = (myname) => {
     return p("&copy; 2023 ", myname, ". All rights reserved.")
 }
